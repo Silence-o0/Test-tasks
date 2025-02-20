f020c2e658a79ab6bfee7a589baf63f06eb432f1
Olga <114361724+Silence-o0@users.noreply.github.com>
2025-02-20 17:01:56 +0200

    Update classifier.py

--- Task1/classifier.py
+++ Task1/classifier.py
@@ -76,7 +76,8 @@ class CNNMnist(MnistClassifierInterface):
     '''
     def __init__(self):
         self.model = Sequential([
-            Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
+            Input(shape=(28, 28, 1)),
+            Conv2D(32, kernel_size=(3, 3), activation='relu'),
             MaxPooling2D(pool_size=(2, 2)),
             Flatten(),
             Dense(128, activation='relu'),
